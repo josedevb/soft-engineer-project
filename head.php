3be8cae4fc12b7051ab6184f059d1ebacc87b12b
jozekcore <josek777@gmail.com>
2018-03-24 23:05:28 -0430

    Add Tables and response order funtions

--- head.php
+++ head.php
@@ -4,11 +4,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-  <link rel="stylesheet" type="text/css" href="css/estilos.css">
+  	<link rel="stylesheet" type="text/css" href="css/estilos.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-  <script src="js/jquery.js"></script>
-  <script src="js/index.js"></script>
-
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+	<link href="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-responsive.min.css" rel="stylesheet">
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+  	<script src="js/jquery.js"></script>
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+  	<script src="js/index.js"></script>
+  	<script src="js/mindmup-editabletable.js"></script>
+  	<script src="js/numeric-input-example.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
 	<script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
--- responderorden.php
+++ responderorden.php
@@ -4,6 +4,13 @@
   $id = $_SESSION['idusuario'];
   $id_orden = $_GET['id'];
 ?>
+
+<script>
+  $('#mainTable').editableTableWidget().numericInputExample().find('td:first').focus();
+  $('#textAreaEditor').editableTableWidget({editor: $('<textarea>')});
+  window.prettyPrint && prettyPrint();
+</script>
+
 <div class="container-fluid">
   <div class="formBox">
     <form name="respondorder_form" action="respond_order.php" method="post" class="form">
@@ -14,7 +21,7 @@
       </div>
 
       <div class="row">
-        <div class="col-sm-12">
+        <div class="col-sm-6">
           <div class="inputBox focus">
             <div class="inputText ">Reparaciones realizadas</div>
             <input type="text" placeholder="1" name="usuario_emisor" value="" class="input">
@@ -24,13 +31,157 @@
             <input type="text" placeholder="5" name="usuario_receptor" value="" class="input">
           </div>
         </div>
+        <div class="col-sm-6">
+          <div class="inputBox focus">
+            <div class="inputText ">Reparaciones realizadas</div>
+            <input type="text" placeholder="6" name="usuario_emisor" value="" class="input">
+            <input type="text" placeholder="7" name="usuario_receptor" value="" class="input">
+            <input type="text" placeholder="8" name="usuario_receptor" value="" class="input">
+            <input type="text" placeholder="9" name="usuario_receptor" value="" class="input">
+            <input type="text" placeholder="10" name="usuario_receptor" value="" class="input">
+          </div>
+        </div>
       </div>
+ 
+      <input style="position: absolute; display: none;">
 
       <div class="row">
         <div class="title-row">
           <h3>Repuestos suministrados</h3>
         </div>
-        <div class="col-sm-3">
+
+          <div style="overflow-x:auto;">
+            <table id="mainTable" class="table" style="width:100%;" style="cursor: pointer;">
+              <thead>
+                <tr>
+                  <th>No.</th>
+                  <th>Cant.</th>
+                  <th>Código</th>
+                  <th>Descripción</th>
+                  <th>Procedencia</th>
+                  <th>Req Mant</th>
+                  <th>Precio unitario</th>
+                  <th>Precio total</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td tabindex="1">1</td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                </tr>
+                <tr>
+                  <td tabindex="1">2</td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                </tr>
+                <tr>
+                  <td tabindex="1">3</td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                </tr>
+                <tr>
+                  <td tabindex="1">4</td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                </tr>
+                <tr>
+                  <td tabindex="1">5</td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                </tr>
+                <tr>
+                  <td tabindex="1">6</td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                </tr>
+                <tr>
+                  <td tabindex="1">7</td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                </tr>
+                <tr>
+                  <td tabindex="1">8</td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                </tr>
+                <tr>
+                  <td tabindex="1">9</td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                </tr>
+                <tr>
+                  <td tabindex="1">10</td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                  <td tabindex="1"></td>
+                </tr>
+              </tbody>
+              <tfoot> 
+                <tr>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+
+        <!-- <div class="col-sm-3">
           <div class="inputBox focus" style="display:flex;">
             <div class="inputText">Cant</div>
               <input type="number" placeholder="nº" name="usuario_receptor" value="" class="input">
@@ -46,7 +197,7 @@
 
         <div class="col-sm-3">
           <div class="inputBox focus">
-            <div class="inputText">Descripcion</div>
+            <div class="inputText">Descripción</div>
             <input type="text" name="mecanico_asignado" placeholder="Ingrese la descripción" required class="input">
           </div>
         </div>
@@ -77,163 +228,146 @@
             <div class="inputText">Precio total</div>
             <input type="text" name="mecanico_asignado" placeholder="Precio total" required class="input">
           </div>
-        </div>
+        </div>-->
 
-        <div class="col-sm-12"></div>
-
-          <div class="col-sm-3">
-          <div class="inputBox focus" style="display:flex;">
-            <div class="inputText">Cant</div>
-              <input type="number" placeholder="nº" name="usuario_receptor" value="" class="input">
-          </div>
-        </div>
-
-        <div class="col-sm-3">
-          <div class="inputBox focus">
-            <div class="inputText">Código</div>
-            <input type="text" name="usuario_equipo" placeholder="Ingrese el código" required class="input">
-          </div>
-        </div>
-
-        <div class="col-sm-3">
-          <div class="inputBox focus">
-            <div class="inputText">Descripcion</div>
-            <input type="text" name="mecanico_asignado" placeholder="Ingrese la descripción" required class="input">
-          </div>
-        </div>
-
-        <div class="col-sm-3">
-          <div class="inputBox focus">
-            <div class="inputText">Procedencia</div>
-            <input type="text" name="mecanico_asignado" placeholder="Ingrese la procedencia" required class="input">
-          </div>
-        </div>
         
-        <div class="col-sm-3">
-          <div class="inputBox focus">
-            <div class="inputText">Req mant</div>
-            <input type="text" name="mecanico_asignado" placeholder="Req mant. nº" required class="input">
-          </div>
-        </div>
-
-        <div class="col-sm-2">
-          <div class="inputBox focus">
-            <div class="inputText">Precio unitario</div>
-            <input type="text" name="mecanico_asignado" placeholder="Precio unitario" required class="input">
-          </div>
-        </div>
-
-        <div class="col-sm-3">
-          <div class="inputBox focus">
-            <div class="inputText">Precio total</div>
-            <input type="text" name="mecanico_asignado" placeholder="Precio total" required class="input">
-          </div>
-        </div>
       </div>
 
       <div class="row">
         <div class="title-row">
           <h3>Reparaciones efectuadas por taller externo</h3>
         </div>
-        <div class="col-sm-3">
-          <div class="inputBox focus" style="display:flex;">
-            <div class="inputText">Cant</div>
-              <input type="number" placeholder="nº" name="usuario_receptor" value="" class="input">
-          </div>
-        </div>
-
-        <div class="col-sm-3">
-          <div class="inputBox focus">
-            <div class="inputText">Código</div>
-            <input type="text" name="usuario_equipo" placeholder="Ingrese el código" required class="input">
-          </div>
-        </div>
-
-        <div class="col-sm-3">
-          <div class="inputBox focus">
-            <div class="inputText">Descripcion</div>
-            <input type="text" name="mecanico_asignado" placeholder="Ingrese la descripción" required class="input">
-          </div>
-        </div>
-
-        <div class="col-sm-3">
-          <div class="inputBox focus">
-            <div class="inputText">Procedencia</div>
-            <input type="text" name="mecanico_asignado" placeholder="Ingrese la procedencia" required class="input">
-          </div>
+        <div style="overflow-x:auto;">
+          <table id="mainTable" class="table" style="width:100%;" style="cursor: pointer;">
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>Cant.</th>
+                <th>Código</th>
+                <th>Descripción</th>
+                <th>Procedencia</th>
+                <th>Req Mant</th>
+                <th>Precio unitario</th>
+                <th>Precio total</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td tabindex="1">1</td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+              </tr>
+              <tr>
+                <td tabindex="1">2</td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+              </tr>
+              <tr>
+                <td tabindex="1">3</td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+              </tr>
+              <tr>
+                <td tabindex="1">4</td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+              </tr>
+              <tr>
+                <td tabindex="1">5</td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+              </tr>
+              <tr>
+                <td tabindex="1">6</td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+              </tr>
+              <tr>
+                <td tabindex="1">7</td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+              </tr>
+              <tr>
+                <td tabindex="1">8</td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+              </tr>
+              <tr>
+                <td tabindex="1">9</td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+              </tr>
+              <tr>
+                <td tabindex="1">10</td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+                <td tabindex="1"></td>
+              </tr>
+            </tbody>
+            <tfoot> 
+              <tr>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+              </tr>
+            </tfoot>
+          </table>
         </div>
         
-        <div class="col-sm-3">
-          <div class="inputBox focus">
-            <div class="inputText">Req mant</div>
-            <input type="text" name="mecanico_asignado" placeholder="Req mant. nº" required class="input">
-          </div>
-        </div>
-
-        <div class="col-sm-2">
-          <div class="inputBox focus">
-            <div class="inputText">Precio unitario</div>
-            <input type="text" name="mecanico_asignado" placeholder="Precio unitario" required class="input">
-          </div>
-        </div>
-
-        <div class="col-sm-3">
-          <div class="inputBox focus">
-            <div class="inputText">Precio total</div>
-            <input type="text" name="mecanico_asignado" placeholder="Precio total" required class="input">
-          </div>
-        </div>
-
-        <div class="col-sm-12"></div>
-
-          <div class="col-sm-3">
-          <div class="inputBox focus" style="display:flex;">
-            <div class="inputText">Cant</div>
-              <input type="number" placeholder="nº" name="usuario_receptor" value="" class="input">
-          </div>
-        </div>
-
-        <div class="col-sm-3">
-          <div class="inputBox focus">
-            <div class="inputText">Código</div>
-            <input type="text" name="usuario_equipo" placeholder="Ingrese el código" required class="input">
-          </div>
-        </div>
-
-        <div class="col-sm-3">
-          <div class="inputBox focus">
-            <div class="inputText">Descripcion</div>
-            <input type="text" name="mecanico_asignado" placeholder="Ingrese la descripción" required class="input">
-          </div>
-        </div>
-
-        <div class="col-sm-3">
-          <div class="inputBox focus">
-            <div class="inputText">Procedencia</div>
-            <input type="text" name="mecanico_asignado" placeholder="Ingrese la procedencia" required class="input">
-          </div>
-        </div>
-        
-        <div class="col-sm-3">
-          <div class="inputBox focus">
-            <div class="inputText">Req mant</div>
-            <input type="text" name="mecanico_asignado" placeholder="Req mant. nº" required class="input">
-          </div>
-        </div>
-
-        <div class="col-sm-2">
-          <div class="inputBox focus">
-            <div class="inputText">Precio unitario</div>
-            <input type="text" name="mecanico_asignado" placeholder="Precio unitario" required class="input">
-          </div>
-        </div>
-
-        <div class="col-sm-3">
-          <div class="inputBox focus">
-            <div class="inputText">Precio total</div>
-            <input type="text" name="mecanico_asignado" placeholder="Precio total" required class="input">
-          </div>
-        </div>
       </div>
 
       <div class="row">
@@ -253,5 +387,11 @@
     </form>
   </div>
 </div>
+
+<script>
+  $('#mainTable').editableTableWidget().numericInputExample().find('td:first').focus();
+  $('#textAreaEditor').editableTableWidget({editor: $('<textarea>')});
+  window.prettyPrint && prettyPrint();
+</script>
 
 <div class="row clearfix" style="height: 5vh"></div>
